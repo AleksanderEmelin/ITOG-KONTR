81d45fa90f5782ccd1695f0c0621114e16c5e966
Emelin <emelin.ss@mail.ru>
2023-09-15 19:40:56 +0300

    Объявляем два массива

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+       string[] array2 = new string[array1.Length];
+       int count = 0; 
+    }
+}
